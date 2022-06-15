9e5e8bb765b206bc1faf4f0b998760a34a28dd44
Thore <91696552+ThoreEI@users.noreply.github.com>
2022-06-15 09:45:27 -0500

    Update ImageProcessing.h

--- ImageProcessing.h
+++ ImageProcessing.h
@@ -38,7 +38,7 @@ public:
             // write the body
             if (0 < countOfPixel && countOfPixel % header.width == 0) // writing a new line if the current line ends
                 fprintf (grayedOutPpmImage, "%c", '\n');
-            fprintf(grayedOutPpmImage, "%d %d %d ", grayedOutPixel, grayedOutPixel, grayedOutPixel); // three times R,G,B -> Grey, Grey, Grey
+            fprintf(grayedOutPpmImage, "%d ", grayedOutPixel); // three times R,G,B -> Grey, Grey, Grey
         }
         fclose(coloredPpmImage);
         fclose(grayedOutPpmImage);
@@ -50,47 +50,110 @@ public:
 
         readAndPrintHeaderOfPpmImage(grayedOutPpmImage, edgeFilteredPpmImage);
 
-        int pixelData[header.height][header.width*3];
-        for (int height = 0; height < header.height; height++)
-        {
-            for (int width = 0; width < header.width * 3; width++)
-            {
+        // fill 2D Array with read body-information
+        int pixelData[header.height][header.width];
+        for (int height = 0; height < header.height; height++) {
+            for (int width = 0; width < header.width; width++) {
                 int currentPixel;
                 fscanf(grayedOutPpmImage, "%d", &currentPixel);
                 pixelData[height][width] = currentPixel;
             }
         }
 
-        int filter[3][3] = {{-1, -1, -1},
-                            {-1, 8,  -1},
-                            {-1, 1,  -1}};
-
-        for (int height = 0; height < header.height; height++) {
+        for (int height = 0; height < header.height; height++)
+        {
             if (0 < height)
                 fprintf(edgeFilteredPpmImage, "%c", '\n');
-            for (int width = 0; width < header.width * 3; width++)
+            for (int width = 0; width < header.width; width++)
             {
-                if (pixelData[height][width] > 0
-                    && height > 1
-                    && height < header.height
-                    && width > 0
-                    && width < header.width)
-                {
-                    int currentPixel = pixelData[height][width];
-                    currentPixel *= filter[1][1]; // multiplied by 8
-                    currentPixel += (filter[0][0] * pixelData[height - 1][width - 1]);
-                    currentPixel += (filter[0][1] * pixelData[height - 1][width]);
-                    currentPixel += (filter[0][2] * pixelData[height - 1][width + 1]);
-                    currentPixel += (filter[1][0] * pixelData[height][width - 1]);
-                    currentPixel += (filter[1][2] * pixelData[height][width + 1]);
-                    currentPixel += (filter[2][0] * pixelData[height + 1][width - 1]);
-                    currentPixel += (filter[2][1] * pixelData[height + 1][width]);
-                    currentPixel += (filter[2][2] * pixelData[height - 1][width + 1]);
-                    if (currentPixel > 0 && currentPixel < 10)
-                        pixelData[height][width] = currentPixel;
 
+                int currentPixel = pixelData[height][width];
+
+                if (currentPixel > 0 && width > 0 && width < header.width && height > 0 && height < header.height) {
+
+                    int up = height - 1;
+                    int down = height + 1;
+                    int left = width - 1;
+                    int right = width + 1;
+
+
+
+                    currentPixel *= 8;
+                    currentPixel += (pixelData[up][left] * -1 + pixelData[up][width] *-1 + pixelData[up][right] * -1 + pixelData[height][left] *-1
+                                    + pixelData[height][right] *-1 + pixelData[down][left] * -1 + pixelData[down][width] *-1 + pixelData[down][right] *-1);
+
+                    if (currentPixel < 0)
+                        currentPixel = 0;
+
+
+                    /*
+
+                         int calculationValues[9];
+                    for (int i = 0; i < sizeof(calculationValues) / sizeof(calculationValues[0]); i++) {
+                        calculationValues[i] = 0;
+                    }
+                    bool validUp, validLeft, validRight, validDown;
+
+                    validUp = height > 0;
+                    validDown = height != header.height;
+                    validLeft = width > 0;
+                    validRight = width != header.width;
+
+                    calculationValues[0] = pixelData[height][width] * 8;
+
+                    int newPixelValue = 0;
+
+                    if (calculationValues[0] != 0) {
+
+                        if (validUp) {
+                            calculationValues[1] = pixelData[up][width] * -1;
+
+                            if (validLeft) {
+                                calculationValues[2] = pixelData[up][left] * -1;
+                            }
+
+                            if (validRight) {
+                                calculationValues[3] = pixelData[up][right] * -1;
+                            }
+                        }
+
+                        if (validDown) {
+                            calculationValues[4] = pixelData[down][width] * -1;
+
+                            if (validLeft) {
+                                calculationValues[5] = pixelData[down][left] * -1;
+                            }
+
+                            if (validRight) {
+                                calculationValues[6] = pixelData[down][right] * -1;
+                            }
+                        }
+
+                        if (validLeft) {
+                            calculationValues[7] = pixelData[height][left] * -1;
+                        }
+
+                        if (validRight) {
+                            calculationValues[8] = pixelData[height][right] * -1;
+                        }
+
+                        for (int pixelValue: calculationValues) {
+                            newPixelValue += pixelValue;
+                        }
+                    } else {
+                        newPixelValue = 0;
+                    }
+                     if (newPixelValue / 9 < 0)
+                        newPixelValue = 0;
+                    fprintf(edgeFilteredPpmImage, "%d ", (newPixelValue / 9));
+                    */
+
+                    fprintf(edgeFilteredPpmImage, "%d ", currentPixel);
+                } else {
+                    fprintf(edgeFilteredPpmImage,"%d ", currentPixel);
                 }
-                fprintf(edgeFilteredPpmImage, "%d ", pixelData[height][width]);
+
+
             }
         }
         fclose(grayedOutPpmImage);
@@ -105,17 +168,11 @@ public:
     }
 
     void readAndPrintHeaderOfPpmImage(FILE * inputFile, FILE * outputFile) {
-        // check the presence of the file
-        if (inputFile == nullptr)
-        {
-            cout << "An error occurred. Could not open the file.";
-            return;
-        }
         // read the header
         fscanf(inputFile, "%s", header.type);
         fscanf(inputFile, "%d %d %d", &header.width, &header.height, &header.brightness);
         //write header
-        fprintf(outputFile, "%s\n", header.type);
+        fprintf(outputFile, "%s\n", "P2");
         fprintf(outputFile, "%d %d\n", header.width, header.height);
         fprintf(outputFile, "%d\n", header.brightness);
     }
