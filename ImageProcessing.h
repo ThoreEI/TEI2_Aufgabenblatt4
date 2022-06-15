2918c23a589ba7d6ab877913f9cdab084c7f76b3
Thore <2120752@stud.hs-mannheim.de>
2022-06-15 17:26:08 +0200

    Code stinkt nicht mehr ganz so stark nach Scheiße

--- ImageProcessing.h
+++ ImageProcessing.h
@@ -77,11 +77,9 @@ public:
                     int right = width + 1;
 
 
-
                     currentPixel *= 8;
                     currentPixel += (pixelData[up][left] * -1 + pixelData[up][width] *-1 + pixelData[up][right] * -1 + pixelData[height][left] *-1
-                                    + pixelData[height][right] *-1 + pixelData[down][left] * -1 + pixelData[down][width] *-1 + pixelData[down][right] *-1);
-
+                                     + pixelData[height][right] *-1 + pixelData[down][left] * -1 + pixelData[down][width] *-1 + pixelData[down][right] *-1);
                     if (currentPixel < 0)
                         currentPixel = 0;
 
--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ int main()
      */
 
     // reading part
-   // string filenameOfPpmImage = "C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//coloredSmiley.ppm";
-    string filenameOfPpmImage = "/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/coloredSmiley.ppm";
+    string filenameOfPpmImage = "C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//coloredPicture.ppm";
+    //string filenameOfPpmImage = "/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/coloredSmiley.ppm";
     string dataOfImage = PPM::readData(filenameOfPpmImage);
     // writing part
-    //string filenameOfNewPpm = "C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//grayedOutSmiley.ppm";
-    string filenameOfNewPpm = "/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/grayedOutSmiley.ppm";
+    string filenameOfNewPpm = "C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//grayedOutPicture.ppm";
+    //string filenameOfNewPpm = "/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/grayedOutSmiley.ppm";
     PPM::writeData(dataOfImage, filenameOfNewPpm);
 
     /*
@@ -24,19 +24,19 @@ int main()
 
     auto imageProcessing = new ImageProcessing();
 
-  //  FILE * coloredPpmImage = fopen("C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//coloredSmiley.ppm", "r");
-    FILE * coloredPpmImage = fopen("/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/coloredSmiley.ppm", "r");
-   // FILE * grayedOutPpmImage = fopen("C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//grayedOutSmiley.ppm", "w");
-    FILE * grayedOutPpmImage = fopen("/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/grayedOutSmiley.ppm", "w");
+    FILE * coloredPpmImage = fopen("C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//coloredPicture.ppm", "r");
+    //FILE * coloredPpmImage = fopen("/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/coloredSmiley.ppm", "r");
+    FILE * grayedOutPpmImage = fopen("C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//grayedOutPicture.ppm", "w");
+    //FILE * grayedOutPpmImage = fopen("/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/grayedOutSmiley.ppm", "w");
 
     imageProcessing->convertToGray(coloredPpmImage,grayedOutPpmImage);
 
     //reopening the file --> resetting pointer & changing mode
-    grayedOutPpmImage = fopen("C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//grayedOutSmiley.ppm", "r");
-    grayedOutPpmImage = fopen("/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/grayedOutSmiley.ppm", "r");
+    grayedOutPpmImage = fopen("C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//grayedOutPicture.ppm", "r");
+    //grayedOutPpmImage = fopen("/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/grayedOutSmiley.ppm", "r");
 
-  //  FILE * edgeFilteredPpmImage = fopen("C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//edgeFilteredSmiley.ppm", "w");
-    FILE * edgeFilteredPpmImage = fopen("/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/edgeFilteredSmiley.ppm", "w");
+    FILE * edgeFilteredPpmImage = fopen("C://Users//UnknownUser//CLionProjects//TEI2_Aufgabenblatt4//files//edgeFilteredPicture.ppm", "w");
+    // FILE * edgeFilteredPpmImage = fopen("/home/user/CLionProjects/TEI2_Aufgabenblatt4/files/edgeFilteredSmiley.ppm", "w");
 
     imageProcessing->edgeDetection(grayedOutPpmImage, edgeFilteredPpmImage);
     return 0;
